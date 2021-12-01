ca2c372ffc573bb4036feef3341c22095971c8bb
Polina Osipova <76431671+PolinaOsi@users.noreply.github.com>
2021-12-02 00:50:46 +0700

    Update lab.c

--- lab.c
+++ lab.c
@@ -14,7 +14,6 @@
 char* msg_about_error_of_lock_mtx = "Error of lock of mutex";
 char* msg_about_error_of_unlock_mtx = "Error of unlock of mutex";
 
-static pthread_mutex_t mutexes[count_of_mutexes];
 bool readiness = false;
 
 int checkOfErrors (int result_of_action, char *info_about_error) {
@@ -24,14 +23,14 @@ int checkOfErrors (int result_of_action, char *info_about_error) {
     }
     return SUCCESS;
 }
-void destroyOfMutexes (int count_of_mtxs) {
+void destroyOfMutexes (int count_of_mtxs, pthread_mutex_t *mutexes) {
     for (int i = 0; i < count_of_mtxs; i++) {
         errno = pthread_mutex_destroy(&mutexes[i]);
         checkOfErrors(errno, "Error of destroying of mutexes");
     }
 }
 
-int initializeOfMutexes () {
+int initializeOfMutexes (pthread_mutex_t *mutexes) {
     pthread_mutexattr_t mattr;
 
     checkOfErrors(errno, "Error of initialization of attributes of mutexes");
@@ -43,7 +42,7 @@ int initializeOfMutexes () {
         errno = pthread_mutex_init(&mutexes[i], &mattr);
         int result_of_init = checkOfErrors(errno, "Error of initialization of mutexes");
         if (result_of_init != SUCCESS) {
-            destroyOfMutexes(count_of_mutexes);
+            destroyOfMutexes(count_of_mutexes, &mutexes[i]);
             exit(EXIT_FAILURE);
         }
     }
@@ -51,19 +50,19 @@ int initializeOfMutexes () {
     return SUCCESS;
 }
 
-int lockOfMutex (int number_of_mtx) {
+int lockOfMutex (int number_of_mtx, pthread_mutex_t *mutexes) {
     errno = pthread_mutex_lock(&mutexes[number_of_mtx]);
     checkOfErrors(errno, msg_about_error_of_lock_mtx);
     return SUCCESS;
 }
 
-int unlockOfMutex (int number_of_mtx) {
+int unlockOfMutex (int number_of_mtx, pthread_mutex_t *mutexes) {
     errno = pthread_mutex_unlock(&mutexes[number_of_mtx]);
     checkOfErrors(errno, msg_about_error_of_unlock_mtx);
     return SUCCESS;
 }
 
-void *printText (void* whois) {
+void *printText (void* whois, pthread_mutex_t *mutexes) {
     char *string = (char*)whois;
     int current_mutex = 0,
         next_mutex = 0;
@@ -71,22 +70,22 @@ void *printText (void* whois) {
     if (!readiness) {
         current_mutex = 2;
 
-        errno = lockOfMutex(current_mutex);
+        errno = lockOfMutex(current_mutex, &mutexes[current_mutex]);
         checkOfErrors(errno, msg_about_error_of_lock_mtx);
         readiness = true;
     }
 
     for (int i = 0; i < count_of_lines; i++) {
         next_mutex = (current_mutex + 1) % count_of_mutexes;
-        errno = lockOfMutex(next_mutex);
+        errno = lockOfMutex(next_mutex, &mutexes[next_mutex]);
         checkOfErrors(errno, msg_about_error_of_lock_mtx);
         printf("%s %d\n", string, i++);
-        errno = unlockOfMutex(current_mutex);
+        errno = unlockOfMutex(current_mutex, &mutexes[current_mutex]);
         checkOfErrors(errno, msg_about_error_of_unlock_mtx);
         current_mutex = next_mutex;
     }
 
-    errno = unlockOfMutex(current_mutex);
+    errno = unlockOfMutex(current_mutex, &mutexes[current_mutex]);
     checkOfErrors(errno, msg_about_error_of_unlock_mtx);
     readiness = false;
 }
@@ -94,38 +93,53 @@ void *printText (void* whois) {
 
 int main (int  argc, char *argv[]) {
     pthread_t id_of_thread;
+    pthread_mutex_t mutexes[count_of_mutexes];
     char *text_of_parent = "Parent: ";
     char *text_of_child = "Child: ";
     int first_mutex = 0;
 
-    errno = initializeOfMutexes();
-    checkOfErrors(errno, "Error of initialization of attributes of mutexes");
+    printf("pisya\n");
 
-    errno = lockOfMutex(first_mutex);
-    int result_of_lock = checkOfErrors(errno, msg_about_error_of_lock_mtx);
-    if (result_of_lock != SUCCESS) {
-        destroyOfMutexes(count_of_mutexes);
+    errno = initializeOfMutexes(mutexes);
+    int result_of_init = checkOfErrors(errno, "Error of initialization of attributes of mutexes");
+    if (result_of_init != SUCCESS) {
         exit(EXIT_FAILURE);
     }
+    printf("pisya2\n");
+
+    errno = lockOfMutex(first_mutex, mutexes);
+    int result_of_lock = checkOfErrors(errno, msg_about_error_of_lock_mtx);
+    if (result_of_lock != SUCCESS) {
+        destroyOfMutexes(count_of_mutexes, mutexes);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("pisya3\n");
 
     errno = pthread_create(&id_of_thread, NULL, printText, text_of_child);
     int result_of_creating = checkOfErrors(errno, "Error of creating of thread");
     if (result_of_creating != SUCCESS) {
-        destroyOfMutexes(count_of_mutexes);
+        destroyOfMutexes(count_of_mutexes, mutexes);
         exit(EXIT_FAILURE);
     }
 
+    printf("pisya4\n");
+
     while (readiness != true) {}
 
-    printText(text_of_parent);
+    printText(text_of_parent, mutexes);
+
+    printf("pisya5\n");
 
     errno = pthread_join(id_of_thread, NULL);
     int result_of_joining = checkOfErrors(errno, "Error of joining of thread");
     if (result_of_joining != SUCCESS) {
-        destroyOfMutexes(count_of_mutexes);
+        destroyOfMutexes(count_of_mutexes, mutexes);
         exit(EXIT_FAILURE);
     }
 
-    destroyOfMutexes(count_of_mutexes);
+    printf("pisya6\n");
+
+    destroyOfMutexes(count_of_mutexes, mutexes);
     return SUCCESS;
 }
